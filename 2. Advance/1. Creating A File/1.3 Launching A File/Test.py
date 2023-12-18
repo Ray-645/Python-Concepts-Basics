959b2c6fbf7b5a54e9f5a7412ffc08522b4408e6
Raymon S <136154556+Ray-645@users.noreply.github.com>
2023-12-18 14:08:49 -0500

    Add files via upload

---
+++ 2. Advance/1. Creating A File/1.3 Launching A File/Test.py
@@ -0,0 +1,8 @@
+import os
+# Raymon Shields
+# 10/24/2023
+# Lauching a music file
+
+response = os.startfile(r"Z:\private\Documents\Python Projects\[Test Programs]\Launching A File/Modern Vintage Gamer (MVG)/Pacific Drive - End of Summer Remix.mp3")
+
+print(response)
